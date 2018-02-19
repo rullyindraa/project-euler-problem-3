eb85b6509a03f9f8700760031025adc04d46a902
rullyindraa <rullyindraa@gmail.com>
2018-02-19 11:12:54 +0700

    edit file solution.test.js

--- test/solution.test.js
+++ test/solution.test.js
@@ -1,4 +1,13 @@
+const each= require('jest-each');
+
 const largestPrime = require('../src/solution');
-test('faktor bilangan prima terbesar dari 600851475143 adalah', () => {
-  expect(largestPrime(600851475143)).toBe(6857);
-})
+
+describe('.largestPrime()', () => {
+  each([
+    [600851475143, 6857],
+    [13195, 29],
+    [49, 7],
+  ]).test('make sure the output is correct for the given sample', (a, expected) => {
+      expect(largestPrime(a)).toBe(expected);
+    });
+});
